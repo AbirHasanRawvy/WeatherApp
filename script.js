12c19efff1613cc27f6d3042169a5b3715c73cc9
Abir Hasan <abirhasanrawvy@gmail.com>
2026-04-18 02:14:44 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -16,5 +16,42 @@ async function getWeather(){
     let apikey = "9fb5deca63f2e2f9ea83d18c125e5fdb"
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`
 
+    try{
+
+        let response = await fetch(url)
+        if(!response.ok){
+            throw new Error("City not found")
+        }
+
+        let data = await response.json()
+
+        console.log(data);
+
+    } catch(error){
+
+        console.log(error);
+
+    }
     
 }
+
+function displayWeather(data){
+    let {name, weather, timezone, coord } = data
+        let weatherInfo = document.getElementById('weatherInfo')
+weatherInfo.innerHTML = 
+    <div class="last">
+        <h3>Weather in ${name}</h3>
+        <p>Weather timezone ${timezone}</p>
+        <p>Weather description ${weather[0].description}</p>
+        <p>Weather description ${coord.lat}</p>
+        <p>Weather description ${coord.lon}</p>
+    </div> 
+    
+    //  console.log(data);
+}
+
+
+
+
+
+
